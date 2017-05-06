551c88f37d83d648a5ee20e6092d9dbb47bc5c6a
Jonathan Hernandez <jbilbo@gmail.com>
2017-05-06 13:09:57 +0200

    Implement pubmed extraction

---
+++ tests/pubmed.test.js
@@ -0,0 +1,28 @@
+/* globals test, expect */
+"use strict";
+
+const pubmed = require("../lib/pubmed");
+
+test("extracts PubMed IDs", () => {
+    expect(pubmed.extract(" 0123\n456\n ")).toEqual(["123", "456"]);
+});
+
+test("does not return outputs with PubMed IDs in DOIs", () => {
+    var text = "10.1038/nplants.2015.3\n10.1126/science.286.5445.1679e";
+
+    expect(pubmed.extract(text)).toEqual([]);
+});
+
+test("strips leading 0s", () => {
+    var text = "0000010203\n000456000";
+
+    expect(pubmed.extract(text)).toEqual(["10203", "456000"]);
+});
+
+test("does not consider 0 as a valid Pubmed ID", () => {
+    expect(pubmed.extract("00000000")).toEqual([]);
+});
+
+test("extracts PubMed IDs separated by Unicode whitespace", () => {
+    expect(pubmed.extract("123 456")).toEqual(["123", "456"]);
+});
